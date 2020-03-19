18aeda4af3b677483e96d32e30fa55d470cbb34e
tjddls1728 <61854982+tjddls1728@users.noreply.github.com>
2020-03-19 10:56:22 +0900

    데이터바인딩 사용

--- app/src/main/java/kr/co/a20200319_01_fregmant/FirstFregment.java
+++ app/src/main/java/kr/co/a20200319_01_fregmant/FirstFregment.java
@@ -7,33 +7,30 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import kr.co.a20200319_01_fregmant01.R;
+import kr.co.a20200319_01_fregmant01.databinding.FragmentFirstBinding;
 
 public class FirstFregment extends Fragment {
+
+    FragmentFirstBinding binding = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first,container,false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first,container,false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-//        액티비티의 onCreate에 대응되는 메쏘드.
-        setupEvents();
-        setupValues();
-
     }
 
-    private void setupValues() {
-    }
 
-    void setupEvents() {
-    }
 
 
 
