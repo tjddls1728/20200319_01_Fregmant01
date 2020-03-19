7d92ef6a52e792ceb73a4a0f8c99d1f00f7917cb
tjddls1728 <61854982+tjddls1728@users.noreply.github.com>
2020-03-19 10:13:38 +0900

    두번째 프래그먼트 자바 작성

--- app/src/main/java/kr/co/a20200319_01_fregmant/secondFragment.java
+++ app/src/main/java/kr/co/a20200319_01_fregmant/secondFragment.java
@@ -1,9 +1,38 @@
 package kr.co.a20200319_01_fregmant;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import kr.co.a20200319_01_fregmant01.R;
 
 public class secondFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_second,container,false);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setupEvents();
+        setupValue();
+    }
+
+    private void setupValue() {
+    }
+
+    private void setupEvents(){
+
+    }
 
 
 }
